219c5f0de6cc2c867e49f821769f1cbd8763d9e2
DafaCr <dafacaru@gmail.com>
2025-06-22 11:46:37 -0500

    segundo commit agregar usuario

--- trabajouni.py
+++ trabajouni.py
@@ -23,8 +23,15 @@ def prestar_libro(usuario, titulo, fecha):
         if libro["titulo"] == titulo and libro["disponible"]:
             libro["disponible"] = False
             prestamos.append({"usuario": usuario, "libro": titulo, "fecha": fecha})
-            historial.append(f"{usuario} prestó el libro '{titulo}' el {fecha}")
+            historial.append(f"{usuario} presto el libro '{titulo}' el {fecha}")
             return
         
 def buscar_libro(texto):
     return [libro for libro in libros if texto.lower() in libro["titulo"].lower()]
+
+def agregar_usuario(nombre):
+        for usuario in usuarios:
+            if nombre not in usuarios:
+                usuarios.append(nombre)
+                historial.append(f"{nombre} se registro como usuario")
+                
